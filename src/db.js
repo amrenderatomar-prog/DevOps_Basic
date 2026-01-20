8e4e895799cf0fee29089b3f5584bf7a4d86bf2c
Amrendera Tomar <amrendera.tomar@antino.com>
2026-01-20 14:38:34 +0530

    feat: implement the backend crud api

---
+++ src/db.js
@@ -0,0 +1,23 @@
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
+
+
+pool.connect((err, client, release) => {
+  if (err) {
+    console.error('Database connection failed:', err.message);
+  } else {
+    console.log('Database connected successfully!');
+    release();
+  }
+});
+
+
