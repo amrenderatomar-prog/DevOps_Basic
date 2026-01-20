a7532a2d6b60cd3aa09b2a690659962e8b31355d
Amrendera Tomar <amrendera.tomar@antino.com>
2026-01-20 15:22:38 +0530

    feat: run postgres in docker with persistent volume

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import taskRoutes from './routes/task.route.js';
 
-dotenv.config();
+dotenv.config({ path: '../.env' });
 
 const app = express();
 app.use(express.json());
